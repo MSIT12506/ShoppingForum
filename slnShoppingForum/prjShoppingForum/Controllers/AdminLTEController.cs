b0463ac9fad1f303ec4b51956569e1f8a2f003e1
SingYu <meteor8127@gmail.com>
2020-02-19 22:50:25 +0800

    tmp setting for AdminLTE

---
+++ slnShoppingForum/prjShoppingForum/Controllers/AdminLTEController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjShoppingForum.Controllers
+{
+    public class AdminLTEController : Controller
+    {
+        // GET: AdminLTE
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}

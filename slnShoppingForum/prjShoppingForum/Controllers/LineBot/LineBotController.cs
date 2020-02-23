b6100c44c14b045d05312fd7e51aba8c15c48ca8
SingYu <meteor8127@gmail.com>
2020-02-23 23:18:08 +0800

    test for LineBot

---
+++ slnShoppingForum/prjShoppingForum/Controllers/LineBot/LineBotController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjShoppingForum.Controllers.LineBot
+{
+    public class LineBotController : Controller
+    {
+        // GET: LineBot
+        public ActionResult link(string linkToken)
+        {
+            var stream = linkToken;
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(stream);
+            var tokenS = handler.ReadToken(stream) as JwtSecurityToken;
+
+
+
+            return View();
+        }
+    }
+}

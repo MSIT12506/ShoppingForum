6119c3d9e8e04c4c55350781dfd8c4baa3d17e08
可樂助 <meteor8127@gmail.com>
2020-03-04 18:38:25 +0800

    活動功能更新 + 會員前台功能更新

--- slnShoppingForum/prjShoppingForum/Controllers/NewList/QuestionsController.cs
+++ slnShoppingForum/prjShoppingForum/Controllers/NewList/QuestionsController.cs
@@ -59,81 +59,176 @@ namespace tw.com.essentialoil.Controllers
         //前台題目畫面
         public ActionResult Content()
         {
-            var AllQuiz = db.tQuestions.ToList();
-            int quiznum = AllQuiz.Count;
-            //todo沒題目
-            var rnd = new Random();
-            //qq=題目Id
-            int qq = rnd.Next(0, quiznum - 1);
-            var dd = DateTime.UtcNow.AddHours(8);
-            string userDateString = dd.ToString("yyyy-MM-dd");
+            //userId
+            int userId = Convert.ToInt32(Session[UserDictionary.S_CURRENT_LOGINED_USERFID]);
 
-            var userId = Convert.ToInt32(Session[UserDictionary.S_CURRENT_LOGINED_USERFID]);
-            var testList = db.tTests.Where(p => p.fId == userId).FirstOrDefault();
-            //true 不行做 ; false 可以做
-            bool Testflag = true;
+            //設定一個古老時間
+            DateTime oldTime = DateTime.Parse("1900-01-01");
 
-            if (testList == null)
+            //上一次做完的時間，判斷test裡面，score不為null的最大時間
+            var tmpList =  db.tTests.Where(p => p.fId == userId && p.fQuestionScore != null).ToList();
+
+            DateTime dateTime = (tmpList.Count > 0) ? tmpList.Max(p => p.fScoreDate) : oldTime;
+
+            //取出scoreDate的值減掉今天>1
+            DateTime nowTime = DateTime.UtcNow.AddHours(8);
+            string userDateString = nowTime.ToString("yyyy-MM-dd");
+
+            DateTime dt1 = Convert.ToDateTime(dateTime);
+            DateTime dt2 = Convert.ToDateTime(userDateString);
+            TimeSpan span = dt2 - dt1;
+            double dayDiff = span.TotalDays;
+            int diff = Convert.ToInt32(dayDiff);
+
+            //找到今天是否有建立的test，如果沒有，就新增
+            var targetTest = db.tTests.AsEnumerable().Where(p => p.fId == userId &&
+                                                            p.fScoreDate.Year == DateTime.UtcNow.AddHours(8).Year &&
+                                                            p.fScoreDate.Month == DateTime.UtcNow.AddHours(8).Month &&
+                                                            p.fScoreDate.Day == DateTime.UtcNow.AddHours(8).Day).FirstOrDefault();
+
+            if (targetTest == null)
             {
+                //找題目
+                var AllQuiz = db.tQuestions.ToList();
+                int quiznum = AllQuiz.Count;
 
-                //新增一筆到ttest
+                Random rnd = new Random();
+                int quizId = rnd.Next(0, quiznum - 1);
+
                 var tTest = new tTest();
                 tTest.fId = userId;
-                tTest.fQuestionId = qq+1;
-                tTest.fScoreDate = dd;
+                tTest.fQuestionId = quizId + 1;
+                tTest.fScoreDate = nowTime;
+                tTest.fQuestionScore = null;
                 tTest.fTestDiscontinue = false;
                 db.tTests.Add(tTest);
                 try
                 {
-                    db.SaveChanges();
+                    db.SaveChanges();   //做完後，保證這個user今天已經建立test
                 }
                 catch (Exception e)
                 {
                     throw;
                 }
-                Testflag = false;
-            }
-            else
-            {
-                var testTime = testList.fScoreDate;
-                string userTestDateString = testTime.ToString("yyyy-MM-dd");
-                //取出scoreDate的值減掉今天>1
-                DateTime dt1 = Convert.ToDateTime(userTestDateString);
-                DateTime dt2 = Convert.ToDateTime(userDateString);
-                TimeSpan span = dt2 - dt1;
-                double days = span.TotalDays;
-                int diff = Convert.ToInt32(days);
-                if (diff >= 1)
-                {
-                    //新增一筆到ttest
-                    var tTest = new tTest();
-                    tTest.fId = userId;
-                    tTest.fQuestionId = qq + 1;
-                    tTest.fScoreDate = dd;
-                    tTest.fTestDiscontinue = false;
-                    db.tTests.Add(tTest);
-                    try
-                    {
-                        db.SaveChanges();
-                    }
-                    catch (Exception e)
-                    {
-                        throw;
-                    }
-                    Testflag = false;
-                }
-                else
-                {
-                    //true=做過=不行做
-                    Testflag = true;
-                }
-            }
-            if (quiznum > 0  && Testflag == false)  //&& 要寫一個flag)
-            {
-                return View(AllQuiz[qq]);
             }
             
+
+            if (diff>=1)
+            {
+                //只要找這個user,目前在tTest裡面的最後一筆就好
+                tTest targetRow = db.tTests.AsEnumerable().Where(p => p.fId == userId).OrderBy(p=>p.fTestId).LastOrDefault();
+                var QuizId = targetRow.fQuestionId;
+                var AllQuiz = db.tQuestions.ToList();
+                return View(AllQuiz[QuizId - 1]);
+            }
+
             return RedirectToAction("Index", "Home");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //var AllQuiz = db.tQuestions.ToList();
+            //int quiznum = AllQuiz.Count;
+            ////todo沒題目
+            //var rnd = new Random();
+            ////qq=題目Id
+            //int qq = rnd.Next(0, quiznum - 1);
+            //var dd = DateTime.UtcNow.AddHours(8);
+            //string userDateString = dd.ToString("yyyy-MM-dd");
+
+            //var userId = Convert.ToInt32(Session[UserDictionary.S_CURRENT_LOGINED_USERFID]);
+            //var testList = db.tTests.Where(p => p.fId == userId).FirstOrDefault();
+            ////true 不行做 ; false 可以做
+            //bool Testflag = true;
+
+            //if (testList == null)
+            //{
+
+            //    //新增一筆到ttest
+            //    var tTest = new tTest();
+            //    tTest.fId = userId;
+            //    tTest.fQuestionId = qq+1;
+            //    tTest.fScoreDate = dd;
+            //    tTest.fTestDiscontinue = false;
+            //    db.tTests.Add(tTest);
+            //    try
+            //    {
+            //        db.SaveChanges();
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        throw;
+            //    }
+            //    Testflag = false;
+            //}
+            //else
+            //{
+            //    var testTime = testList.fScoreDate;
+            //    string userTestDateString = testTime.ToString("yyyy-MM-dd");
+            //    //取出scoreDate的值減掉今天>1
+            //    DateTime dt1 = Convert.ToDateTime(userTestDateString);
+            //    DateTime dt2 = Convert.ToDateTime(userDateString);
+            //    TimeSpan span = dt2 - dt1;
+            //    double days = span.TotalDays;
+            //    int diff = Convert.ToInt32(days);
+            //    if (diff >= 1)
+            //    {
+            //        //新增一筆到ttest
+            //        var tTest = new tTest();
+            //        tTest.fId = userId;
+            //        tTest.fQuestionId = qq + 1;
+            //        tTest.fScoreDate = dd;
+            //        tTest.fTestDiscontinue = false;
+            //        db.tTests.Add(tTest);
+            //        try
+            //        {
+            //            db.SaveChanges();
+            //        }
+            //        catch (Exception e)
+            //        {
+            //            throw;
+            //        }
+            //        Testflag = false;
+            //    }
+            //    else
+            //    {
+            //        //true=做過=不行做
+            //        Testflag = true;
+            //    }
+            ////}
+            //if (quiznum > 0  && Testflag == false)  //&& 要寫一個flag)
+            //{
+            //    return View(AllQuiz[qq]);
+            //}
+            
         }
 
 

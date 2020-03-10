a80824c50fe2985a4847be7eec757116da20fc60
otonashiame <otonashiame@gmail.com>
2020-03-11 00:57:56 +0800

    2020/3/11 1. 新增討論區標題圖片 2. 修改商品下拉選單、前台搜尋不到商品訊息提示、商品前台進階搜尋動態生表格、商品後台修改圖片顯示、商品後台庫存、商品下架功能、商品新增動態生表格

--- slnShoppingForum/prjShoppingForum/Models/Product/DropDownList.cs
+++ slnShoppingForum/prjShoppingForum/Models/Product/DropDownList.cs
@@ -13,16 +13,20 @@ namespace prjShoppingForum.Models.Entity
         //下拉選單--商品分類
         public IEnumerable<SelectListItem> GetCategoryDropList()
         {
-            var list= db.tCategories.Select(p => new SelectListItem
+            var list = new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                  Text = "請選擇",
+                  Value = null
+                }
+            };
+
+            list.AddRange(db.tCategories.Select(p => new SelectListItem
             {
                 Text = p.fCategoryName,
                 Value = p.fCategoryID.ToString()
-            }).ToList();
-
-            list.Add(new SelectListItem() {
-                Text = "無",
-                Value = null
-            });
+            }).ToList());
 
             return list;
         }
@@ -30,17 +34,20 @@ namespace prjShoppingForum.Models.Entity
         //下拉選單--單方精油萃取部位
         public IEnumerable<SelectListItem> GetPartDropDownList()
         {
-            var list= db.tParts.Select(p => new SelectListItem
+            var list = new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                  Text = "無",
+                  Value = null
+                }
+            };
+
+            list.AddRange(db.tParts.Select(p => new SelectListItem
             {
                 Value = p.fPartID.ToString(),
                 Text = p.fPartName
-            }).ToList();
-
-            list.Add(new SelectListItem()
-            {
-                Text = "無",
-                Value = null
-            });
+            }).ToList());
 
             return list;
         }
@@ -48,17 +55,20 @@ namespace prjShoppingForum.Models.Entity
         //下拉選單--單方精油香調
         public IEnumerable<SelectListItem> GetNoteDropList()
         {
-            var list = db.tNotes.Select(p => new SelectListItem
+            var list = new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Text="無",
+                    Value=null
+                }
+            };
+
+            list.AddRange(db.tNotes.Select(p => new SelectListItem
             {
                 Value = p.fNoteID.ToString(),
                 Text = p.fNoteName,
-            }).ToList();
-
-            list.Add(new SelectListItem()
-            {
-                Text = "無",
-                Value = null
-            });
+            }).ToList());
 
             return list;
         }
@@ -66,36 +76,41 @@ namespace prjShoppingForum.Models.Entity
         //下拉選單--單方&副方精油功效
         public IEnumerable<SelectListItem> GetEfficacyDropLise()
         {
-            var list= db.tEfficacies.Select(p => new SelectListItem
+            var list = new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Text="無",
+                    Value=null
+                }
+            };
+
+            list.AddRange(db.tEfficacies.Select(p => new SelectListItem
             {
                 Value = p.fEfficacyID.ToString(),
-                Text=p.fEfficacyName
-            }).ToList();
-
-            list.Add(new SelectListItem()
-            {
-                Text = "無",
-                Value = null
-            });
+                Text = p.fEfficacyName
+            }).ToList());
 
             return list;
-
         }
 
         //下拉選單--純露/植物油特性
         public IEnumerable<SelectListItem> GetfeatureDropList()
         {
-            var list = db.tfeatures.Select(p => new SelectListItem
+            var list = new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Text="無",
+                    Value=null
+                }
+            };
+
+            list.AddRange(db.tfeatures.Select(p => new SelectListItem
             {
                 Value = p.ffeatureID.ToString(),
                 Text = p.ffeatureName
-            }).ToList();
-
-            list.Add(new SelectListItem()
-            {
-                Text = "無",
-                Value = null
-            });
+            }).ToList());
 
             return list;
         }
@@ -103,22 +118,22 @@ namespace prjShoppingForum.Models.Entity
         //下拉選單--商品功效
         public IEnumerable<SelectListItem> GetEfficacyDropList()
         {
-            var list = db.tEfficacies.Select(p => new SelectListItem
+            var list = new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Text="無",
+                    Value=null
+                }
+            };
+
+            list.AddRange(db.tEfficacies.Select(p => new SelectListItem
             {
                 Value = p.fEfficacyID.ToString(),
                 Text = p.fEfficacyName
-            }).ToList();
-
-            list.Add(new SelectListItem()
-            {
-                Text = "無",
-                Value = null
-            });
+            }).ToList());
 
             return list;
         }
-
-
-
     }
 }
